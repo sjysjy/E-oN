a287563f4a45192b804b269cc38d2252f5b2d2d1
Juyeon Song <jysong0805@naver.com>
2022-05-11 22:16:00 +0900

    7주차 과제

---
+++ 7주차 과제/main.py
@@ -0,0 +1,33 @@
+#main.py
+
+import system
+
+print("도서 관리 프로그램")
+print("1. 도서 추가")
+print("2. 도서 검색")
+print("3. 도서 정보 수정")
+print("4. 도서 삭제")
+print("5. 도서 목록 출력")
+print("6. 종료")
+
+num=input("번호 입력: ")
+
+if num == "1":
+    system.add_Book()
+elif num == "2":
+    system.search_Book()
+elif num == "3":
+    system.edit_Book()
+elif num == "4":
+    system.remove_Book()
+elif num == "5":
+    system.print_Book()
+elif num == "6":
+    print("도서 관리 프로그램을 종료합니다!")
+
+    
+    
+
+    
+    
+    
---
+++ 7주차 과제/system.py
@@ -0,0 +1,90 @@
+# system.py
+# 도서 추가, 검색, 수정, 삭제 함수
+
+
+
+#도서 추가 함수
+def add_Book():
+    book=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "a", encoding='UTF8')
+    add=input("추가할 도서의 도서명, 저자, 출판연도, 출판사명, 장르를 띄어쓰기로 구분하여 입력하세요: ")
+    book.write("\n")
+    book.write(add)
+    book.close()
+
+    check=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+    print(check.read())
+    check.close()
+    
+
+
+#도서 검색 함수
+def search_Book():
+     book=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+     search=input("검색할 단어를 입력하세요: ")
+     lines=book.readlines()
+     for l in lines:
+         if l.find(search) > -1:
+             print(l.strip("\n"))
+         else:
+             pass
+     book.close()
+     
+
+
+#도서 정보 수정 함수
+def edit_Book():
+     book=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+     lines=book.readlines()
+     book_w=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "w", encoding='UTF8')
+     title=input("수정할 도서의 제목을 입력하세요: ")
+     edit=input(" 도서명, 저자, 출판연도, 출판사명, 장르를 띄어쓰기로 구분하여 수정사항을 입력하세요: ")
+     
+     for l in lines:
+         if l.find(title) > -1:
+             book_w.write(edit)
+         else:
+             book_w.write(l)
+     book.close()
+     book_w.close()
+
+     check=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+     print(check.read())
+     check.close()
+    
+
+
+#도서 삭제 함수
+def remove_Book():
+     book_r=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+     lines=book_r.readlines()
+     print(lines)
+     book_w=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "w", encoding='UTF8')
+     title=input("삭제할 도서의 제목을 입력하세요: ")
+     
+     for l in lines:
+         if l.find(title) == -1:
+             book_w.write(l)
+
+     book_r.close()
+     book_w.close()
+         
+
+     check=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+     print(check.read())
+     check.close()
+
+
+
+#현재 도서 목록 출력 함수
+def print_Book():
+    book=open("C:/Users/송주연/AppData/Local/Programs/Python/Python38-32/Python source/E-oN 7th assignment/input.txt", "r", encoding='UTF8')
+    print(book.read())
+    book.close()
+
+
+
+    
+     
+
+             
+
